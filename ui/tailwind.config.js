4204a1e758821e3dca81882e0d1812e9ce0ba12b
Bao <baomai@berkeley.edu>
2023-03-11 14:04:51 -0800

    design system

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'white',
+        secondary: '#A9A9A9',
+        cta: '#FF13CB'
+      }
+    },
   },
   plugins: [],
 }
