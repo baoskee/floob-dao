5a60d3ff1819ff8744184cf633cc9be160db6433
Bao <baomai@berkeley.edu>
2023-03-11 18:46:26 -0800

    github copilot wrote my first test

--- floob-contract/src/contract.rs
+++ floob-contract/src/contract.rs
@@ -121,4 +121,21 @@ fn advance_subthread_count(store: &mut dyn Storage, thread_id: u64) -> StdResult
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*; 
+
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{coins, from_binary, CosmosMsg, WasmMsg};
+
+    #[test]
+    fn proper_initialization() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {};
+        let info = mock_info("creator", &coins(1000, "earth"));
+
+        // we can just call .unwrap() to assert this was a success
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+    } 
+}
